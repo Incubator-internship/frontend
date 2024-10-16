e845539fbcb6b9a8292ac81e1c30f1240c19aea3
Diana <m.diana.dev@gmail.com>
2024-10-16 22:17:43 +0300

    feat: start project

---
+++ src/app/(pages)/signin/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div>
+      <main>Signin Page</main>
+    </div>
+  );
+}
---
+++ src/app/(pages)/signup/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div>
+      <main>Signup Page</main>
+    </div>
+  );
+}
