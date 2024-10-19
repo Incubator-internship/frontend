50af7dafc75b020f2acbb04f8ab79c310d5759a5
Maria Kazakova <mariakazakova92@gmail.com>
2024-10-19 22:34:06 +0300

    feat: add condition and styles for button without border

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -3,14 +3,15 @@ import { ComponentPropsWithoutRef } from 'react'
 import s from './button.module.scss'
 
 type Props = {
+  border?: boolean
   fullWidth?: boolean
   variant?: 'primary' | 'secondary' | 'transparent'
 } & ComponentPropsWithoutRef<'button'>
 
-export function Button({ className, fullWidth, variant = 'primary', ...rest }: Props) {
+export function Button({ border, className, fullWidth, variant = 'primary', ...rest }: Props) {
   return (
     <button
-      className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
+      className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className} ${border ? '' : s.border}`}
       type={'button'}
       {...rest}
     />
