fb972e9100b98b03c98bd86b8c020e933942035f
Maria Kazakova <mariakazakova92@gmail.com>
2024-10-19 21:44:30 +0300

    feat: add button stories

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -3,14 +3,14 @@ import { ComponentPropsWithoutRef } from 'react'
 import s from './button.module.scss'
 
 type Props = {
-  fullwidth?: boolean
+  fullWidth?: boolean
   variant?: 'primary' | 'secondary' | 'transparent'
 } & ComponentPropsWithoutRef<'button'>
 
-export function Button({ className, fullwidth, variant = 'primary', ...rest }: Props) {
+export function Button({ className, fullWidth, variant = 'primary', ...rest }: Props) {
   return (
     <button
-      className={`${s.button} ${s[variant]} ${fullwidth ? s.fullWidth : ''} ${className}`}
+      className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
       type={'button'}
       {...rest}
     />
