a076880a97c14ac4c9e908208932a0059408407b
Kiba Sergey <serhiikiba@gmail.com>
2024-10-20 21:59:40 +0200

    started card

---
+++ src/shared/ui/card/Card.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { ReactNode } from 'react'
+
+import s from './card.module.scss'
+
+type Props = {
+  children: ReactNode
+}
+
+export default function Card({ children }: Props) {
+  return <div className={s.card}>{children}</div>
+}
