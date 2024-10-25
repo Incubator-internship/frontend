4428c5f0e1c8a42553cc2212f8353fe33563cdec
Maria Kazakova <mariakazakova92@gmail.com>
2024-10-25 15:54:31 +0300

    fix: delete references from tsconfig. Add changes to the FormRadioGroup component

--- src/shared/ui/radioGroup/RadioGroup.tsx
+++ src/shared/ui/radioGroup/RadioGroup.tsx
@@ -1,5 +1,6 @@
 import React, { ComponentPropsWithoutRef } from 'react'
 
+import { Typography } from '@/shared/ui/typography'
 import * as RadioGroup from '@radix-ui/react-radio-group'
 
 import s from './radioGroup.module.scss'
@@ -22,32 +23,35 @@ export const FormRadioGroup = ({
   orientation = 'horizontal',
   ...rest
 }: Props) => (
-  <form>
-    <RadioGroup.Root
-      aria-label={'View density'}
-      className={s.RadioGroupRoot}
-      defaultValue={defaultValue}
-      orientation={orientation}
-      style={{ flexDirection: orientation === 'horizontal' ? 'row' : 'column' }}
-      {...rest}
-    >
-      {options.map(option => (
-        <div key={option.value} style={{ alignItems: 'center', display: 'flex' }}>
-          <RadioGroup.Item
-            className={`${s.RadioGroupItem} ${disabled ? s.disabled : ''}`}
-            disabled={disabled}
-            id={'r1'}
-            value={option.value}
-          >
-            <RadioGroup.Indicator
-              className={`${s.RadioGroupIndicator} ${disabled ? s.disabled : ''}`}
-            />
-          </RadioGroup.Item>
-          <label className={`${s.Label} ${disabled ? s.disabled : ''}`} htmlFor={'r1'}>
-            {option.label}
-          </label>
-        </div>
-      ))}
-    </RadioGroup.Root>
-  </form>
+  <RadioGroup.Root
+    aria-label={'View density'}
+    className={s.RadioGroupRoot}
+    defaultValue={defaultValue}
+    orientation={orientation}
+    style={{ flexDirection: orientation === 'horizontal' ? 'row' : 'column' }}
+    {...rest}
+  >
+    {options.map(option => (
+      <div key={option.value} style={{ alignItems: 'center', display: 'flex' }}>
+        <RadioGroup.Item
+          className={`${s.RadioGroupItem} ${disabled ? s.disabled : ''}`}
+          disabled={disabled}
+          id={option.value}
+          value={option.value}
+        >
+          <RadioGroup.Indicator
+            className={`${s.RadioGroupIndicator} ${disabled ? s.disabled : ''}`}
+          />
+        </RadioGroup.Item>
+        <Typography
+          as={'label'}
+          className={`${s.Label} ${disabled ? s.disabled : ''}`}
+          htmlFor={option.value}
+          variant={'caption'}
+        >
+          {option.label}
+        </Typography>
+      </div>
+    ))}
+  </RadioGroup.Root>
 )
