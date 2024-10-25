2fdc99cd1caad93728031ec59fb34b1bdacc5e58
KristinOsmakov <oskristinov@gmail.com>
2024-10-26 00:33:56 +0300

    fix: fixed page.tsx - returned previous state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,13 @@ import Link from 'next/link'
 import { Select } from '../shared/ui/select/Select'
 
 export default function Home() {
-  const item = [
-    { title: 'one', value: '1' },
-    { title: 'two', value: '2' },
-    { title: 'three', value: '3' },
-  ]
-
   return (
     <div>
       <main>
         <div>
-          {/* <Link href={'/signup'}>Signup</Link>
+          <Link href={'/signup'}>Signup</Link>
           <Link href={'/signin'}>Signin</Link>
-          <Link href={'/profile'}>Profile</Link> */}
-          <Select items={item} />
+          <Link href={'/profile'}>Profile</Link>
         </div>
       </main>
     </div>
