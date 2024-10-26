f2d0f97f2a435683f5e57842f60ab9bc8136c694
ArtemRubtsov <ua.rubtsov.a@gmail.com>
2024-10-26 15:41:13 +0300

    feat: added Alert component, story and style

--- src/shared/ui/alerts/Alert.tsx
+++ src/shared/ui/alerts/Alert.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import Close from '@/shared/assets/icons/Close'
 import clsx from 'clsx'
 
 import s from './alert.module.scss'
@@ -7,17 +8,21 @@ import s from './alert.module.scss'
 type AlertProps = {
   children?: React.ReactNode
   className?: string
+  onClose?: (isClosed: boolean) => void
   style?: React.CSSProperties
   title?: string
   type?: 'error' | 'success'
 }
 
 export const Alert = (props: AlertProps) => {
-  const { children, className, style, type, ...rest } = props
+  const { children, className, type = 'success', ...rest } = props
 
   return (
-    <div className={clsx(s.alert, className)} {...rest}>
-      {children}
+    <div className={clsx(s.alert, type && s[type], className)} {...rest}>
+      <p>{children ? children : rest.title}</p>
+      <button type={'button'}>
+        <Close className={clsx(s.close, className)}></Close>
+      </button>
     </div>
   )
 }
--- src/shared/ui/alerts/alert.stories.tsx
+++ src/shared/ui/alerts/alert.stories.tsx
@@ -11,7 +11,30 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const AlertVariant: Story = {
+  render: () => {
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '30px' }}>
+        <div>
+          <Alert title={'Your settings are saved'} type={'success'} />
+        </div>
+        <div>
+          <Alert title={'Error! Server is not available'} type={'error'} />
+        </div>
+      </div>
+    )
+  },
+}
+
+export const AlertSuccess: Story = {
   args: {
+    title: 'Your settings are saved',
     type: 'success',
+  },
+}
+
+export const AlertError: Story = {
+  args: {
+    title: 'Error! Server is not available',
+    type: 'error',
   },
 }
