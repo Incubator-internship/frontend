8ce7cbbd60897ed6b8fbc64154967af24865b8fd
dzlek <6894255@gmail.com>
2024-11-04 17:48:10 +0300

    refactor: merge from dev

--- src/shared/ui/form/forgotPassword/ForgotPasswordForm.tsx
+++ src/shared/ui/form/forgotPassword/ForgotPasswordForm.tsx
@@ -3,29 +3,42 @@ import { useForm } from 'react-hook-form'
 
 import s from './ForgotPasswordForm.module.scss'
 
+import { Button } from '../../button'
 import { InputControl } from '../../inputControl'
 import Recaptcha from '../../recaptcha/Recaptcha'
+import { Typography } from '../../typography/Typography'
 
-type ForgotPasswordFormProps = {
-  sitekey?: string
+type ForgotPasswordFields = {
+  email: string
 }
 
-export const ForgotPasswordForm = ({
-  sitekey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ||
-    '6LeYP3QqAAAAAESr4XvYoiQ40gZHerd5UIpp1oFR',
-  // FIX: storybook don't have access to .env
-}: ForgotPasswordFormProps) => {
-  const { control, reset, setError, setValue, trigger } = useForm<any>({
+export type ForgotPasswordFormProps = {}
+
+export const ForgotPasswordForm = () => {
+  const sitekey =
+    process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || '6LeYP3QqAAAAAESr4XvYoiQ40gZHerd5UIpp1oFR'
+
+  const { control } = useForm<ForgotPasswordFields>({
     defaultValues: {
       email: '',
-      token: '',
     },
   })
 
   return (
     <form className={s.forgotPasswordForm}>
       <h1>Forgot Password</h1>
-
+      <InputControl
+        control={control}
+        label={'Email'}
+        name={'email'}
+        placeholder={'Epam@epam.com'}
+      />
+      <Typography color={'grey'} variant={'regularText14'}>
+        Enter your email address and we will send you further instructions{' '}
+      </Typography>
+      <Button fullWidth variant={'primary'}>
+        Send Link
+      </Button>
       <Recaptcha sitekey={sitekey} theme={'dark'} />
     </form>
   )
