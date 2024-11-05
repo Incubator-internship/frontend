aaad169588b8edeb5dfbcdb67c47043c7ecbdb51
dzlek <6894255@gmail.com>
2024-11-05 20:05:48 +0300

    refactor: merge from dev

--- src/shared/ui/forms/forgotPassword/ForgotPasswordForm.tsx
+++ src/shared/ui/forms/forgotPassword/ForgotPasswordForm.tsx
@@ -42,11 +42,6 @@ export const ForgotPasswordForm = () => {
     setIsModalOpen(true)
   })
 
-  const closeModal = () => {
-    setIsModalOpen(false)
-    setIsSent(true)
-  }
-
   return (
     <form className={s.forgotPasswordForm} onSubmit={onSubmit}>
       <Typography className={s.title} color={'grey'} variant={'h1'}>
@@ -65,18 +60,13 @@ export const ForgotPasswordForm = () => {
       <Typography color={'grey'} variant={'regularText14'}>
         Enter your email address and we will send you further instructions{' '}
       </Typography>
-      {isSent && (
-        <Typography color={'white'} variant={'regularText14'}>
-          The link has been sent by email. If you don’t receive an email send link again
-        </Typography>
-      )}
       <Button fullWidth type={'submit'} variant={'primary'}>
-        {`Send Link${isSent ? ' Again' : ''}`}
+        Send Link
       </Button>
       <Button as={Link} fullWidth href={'/signin'} variant={'transparent'}>
         Back to Sign In
       </Button>
-      {!isSent && <Recaptcha sitekey={sitekey} />}
+      <Recaptcha sitekey={sitekey} />
       {isModalOpen && (
         <div
           style={{
@@ -93,7 +83,7 @@ export const ForgotPasswordForm = () => {
           }}
         >
           MODAL
-          <Button onClick={() => closeModal()}>Close</Button>
+          <Button onClick={() => setIsModalOpen(false)}>Close</Button>
         </div>
       )}
     </form>
