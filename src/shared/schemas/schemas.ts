74b79430f4274f92fae95bdd656f0346f1a565fc
Diana <m.diana.dev@gmail.com>
2024-11-05 17:42:29 +0300

    feat: add signUp form

---
+++ src/shared/schemas/schemas.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod'
+
+const PASSWORD_REQUIREMENTS_MESSAGE =
+  'Password must contain 0-9, a-z, A-Z, and at least one of the following symbols: ! # $ % & ( ) * + , - . / : ; < = > ? @ [ \\ ] _` { | } ~'
+
+export const usernameSchema = z
+  .string()
+  .min(6, { message: 'Minimum number of characters 6' })
+  .max(30, { message: 'Maximum number of characters 30' })
+  .regex(/^[A-Za-z0-9_-]+$/, {
+    message: 'Username must contain 0-9, a-z, A-Z, _ -',
+  })
+
+export const emailSchema = z
+  .string()
+  .email({ message: 'The email must match the format example@example.com' })
+
+export const passwordSchema = z
+  .string()
+  .min(6, { message: 'Minimum number of characters 6' })
+  .max(20, { message: 'Maximum number of characters 20' })
+  .refine(val => /[0-9]/.test(val), {
+    message: PASSWORD_REQUIREMENTS_MESSAGE,
+  })
+  .refine(val => /[A-Z]/.test(val), {
+    message: PASSWORD_REQUIREMENTS_MESSAGE,
+  })
+  .refine(val => /[a-z]/.test(val), {
+    message: PASSWORD_REQUIREMENTS_MESSAGE,
+  })
+  .refine(val => /[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/.test(val), {
+    message: PASSWORD_REQUIREMENTS_MESSAGE,
+  })
+
+export const agreeSchema = z.boolean().refine(val => val, {
+  message: 'You must agree to the terms',
+})
