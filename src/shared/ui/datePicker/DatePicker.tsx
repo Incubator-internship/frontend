065a8d581ab95350c25a7ac87789299ef27190ae
Diana <m.diana.dev@gmail.com>
2024-11-07 15:37:59 +0300

    feat: add date picker with control

--- src/shared/ui/datePicker/DatePicker.tsx
+++ src/shared/ui/datePicker/DatePicker.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentPropsWithoutRef } from 'react'
+import React, { ComponentPropsWithoutRef, ElementRef } from 'react'
 import ReactDatePicker from 'react-datepicker'
 
 import { DataPickerInput } from '@/shared/ui/datePicker/datePickerInput/DatePickerInput'
@@ -10,42 +10,38 @@ import './datePicker.scss'
 
 import s from './datePicker.module.scss'
 
-type DatePickerProps = {
+export type DatePickerProps = {
   error?: string
   label?: string
 } & ComponentPropsWithoutRef<typeof ReactDatePicker>
 
-export const DatePicker = ({
-  className,
-  disabled,
-  error,
-  label,
-  startDate,
-  ...restProps
-}: DatePickerProps) => {
-  return (
-    <div className={clsx(className, disabled ? s.disabled : '')}>
-      {label && (
-        <Typography as={'label'} className={s.label} variant={'body2'}>
-          {label}
-        </Typography>
-      )}
-      <ReactDatePicker
-        calendarStartDay={1}
-        customInput={<DataPickerInput error={error} />}
-        dateFormat={'dd/MM/yyyy'}
-        disabled={disabled}
-        popperPlacement={'top-start'}
-        selected={startDate}
-        showPopperArrow={false}
-        startDate={startDate}
-        {...restProps}
-      />
-      {error && (
-        <Typography as={'span'} className={s.error} variant={'caption'}>
-          {error}
-        </Typography>
-      )}
-    </div>
-  )
-}
+export const DatePicker = React.forwardRef<ElementRef<typeof ReactDatePicker>, DatePickerProps>(
+  ({ className, disabled, error, label, startDate, ...restProps }, ref) => {
+    return (
+      <div className={clsx(className, disabled ? s.disabled : '')}>
+        {label && (
+          <Typography as={'label'} className={s.label} variant={'body2'}>
+            {label}
+          </Typography>
+        )}
+        <ReactDatePicker
+          calendarStartDay={1}
+          customInput={<DataPickerInput error={error} />}
+          dateFormat={'dd/MM/yyyy'}
+          disabled={disabled}
+          popperPlacement={'top-start'}
+          ref={ref}
+          selected={startDate}
+          showPopperArrow={false}
+          startDate={startDate}
+          {...restProps}
+        />
+        {error && (
+          <Typography as={'span'} className={s.error} variant={'caption'}>
+            {error}
+          </Typography>
+        )}
+      </div>
+    )
+  }
+)
---
+++ src/shared/ui/datePickerControl/DatePickerControl.tsx
@@ -0,0 +1,35 @@
+import { FieldValues, UseControllerProps, useController } from 'react-hook-form'
+
+import { DatePicker, DatePickerProps } from '@/shared/ui/datePicker'
+
+type Props<T extends FieldValues> = Omit<
+  DatePickerProps,
+  'disabled' | 'name' | 'onBlur' | 'onChange' | 'ref' | 'selected' | 'value'
+> &
+  UseControllerProps<T>
+
+export const DatePickerControl = <T extends FieldValues>({
+  control,
+  defaultValue,
+  disabled,
+  name,
+  rules,
+  shouldUnregister,
+  ...restProps
+}: Props<T>) => {
+  const {
+    field: { value, ...restField },
+    fieldState: { error },
+  } = useController({
+    control,
+    defaultValue,
+    disabled,
+    name,
+    rules,
+    shouldUnregister,
+  })
+
+  debugger
+
+  return <DatePicker {...restProps} error={error?.message} selected={value} {...restField} />
+}
