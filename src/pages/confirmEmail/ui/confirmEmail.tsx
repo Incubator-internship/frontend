f3ffa368fec5778d70d3812ab9dc16f7c56308cd
Diana <m.diana.dev@gmail.com>
2024-11-19 16:27:02 +0300

    feat: add layout confirm email page

---
+++ src/pages/confirmEmail/ui/confirmEmail.tsx
@@ -0,0 +1,23 @@
+import ConfirmEmailSuccess from '@/shared/assets/icons/confirmEmailSuccess'
+import { Button } from '@/shared/ui/button'
+import { Typography } from '@/shared/ui/typography'
+import Link from 'next/link'
+
+import s from './confirmEmail.module.scss'
+
+export default function confirmEmail() {
+  return (
+    <div className={s.page}>
+      <Typography as={'h1'} className={s.title} variant={'h1'}>
+        Congratulations!
+      </Typography>
+      <Typography as={'p'} className={s.subtitle} variant={'regularText16'}>
+        Your email has been confirmed
+      </Typography>
+      <Button as={Link} className={s.button} href={'/signin'}>
+        Sign In
+      </Button>
+      <ConfirmEmailSuccess className={s.image} />
+    </div>
+  )
+}
