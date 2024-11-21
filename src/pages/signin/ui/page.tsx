d752f64e9c5ec3f6eb062cddd0d4ad125f56c57f
Maria Kazakova <mariakazakova92@gmail.com>
2024-11-21 16:07:35 +0300

    feat: depending on the conditions, the login page or loading or transition to the profile page is rendered

--- src/pages/signin/ui/page.tsx
+++ src/pages/signin/ui/page.tsx
@@ -5,6 +5,7 @@ import { useEffect } from 'react'
 import { LoginData, useLoginMutation } from '@/app/api/signInApi'
 import { Schema, SignInForm } from '@/shared/ui/forms/signIn'
 import { clsx } from 'clsx'
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 
 import s from './signInPage.module.scss'
@@ -25,20 +26,23 @@ export default function SignInPage() {
   useEffect(() => {
     if (isSuccess) {
       localStorage.setItem('accessToken', data.accessToken)
-      router.push('/profile')
     }
   }, [data, isSuccess, router])
 
-  return (
-    <main className={clsx(s.pageContainer)}>
-      {isLoading ? (
-        //TODO add the loading component when it's ready
-        <div>Loading...</div>
-      ) : (
+  const renderContent = () => {
+    if (isLoading) {
+      return <div className={clsx(s.loading)}>Loading...</div>
+    }
+    if (isSuccess) {
+      return <Link href={'/profile'} />
+    } else {
+      return (
         <div className={clsx(s.formWrapper)}>
           <SignInForm isError={isError} onSubmit={handleSubmit} />
         </div>
-      )}
-    </main>
-  )
+      )
+    }
+  }
+
+  return <main className={clsx(s.pageContainer)}>{renderContent()}</main>
 }
