5bbb400682789273b5fd97fa85c1dc9b85c9a990
Diana <m.diana.dev@gmail.com>
2024-11-21 21:20:43 +0300

    feat: add resend verification form

---
+++ src/shared/ui/forms/resendVerification/resendVerification.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import {
+  ResendVerificationForm,
+  ResendVerificationFormValues,
+} from '@/shared/ui/forms/resendVerification/ResendVerification'
+
+const meta = {
+  component: ResendVerificationForm,
+  tags: ['autodocs'],
+  title: 'forms/ResendVerification',
+} satisfies Meta<typeof ResendVerificationForm>
+
+export default meta
+type Story = StoryObj<typeof meta>
+const handleSignUp = (data: ResendVerificationFormValues) => {
+  console.log('Form Data:', data)
+}
+
+export const ResendVerification: Story = {
+  args: {
+    onSubmit: handleSignUp,
+  },
+}
