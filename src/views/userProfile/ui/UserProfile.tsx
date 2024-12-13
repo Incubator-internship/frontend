8935d96e780e1d1897e959b024f426038be39218
Artem Rubtsov <ua.rubtsov.a@gmail.com>
2024-12-13 18:18:37 +0200

    feat: userProfile

--- src/views/userProfile/ui/UserProfile.tsx
+++ src/views/userProfile/ui/UserProfile.tsx
@@ -1,46 +1,69 @@
 import React from 'react'
 
+import image1 from '@/shared/assets/images/userProfile/photo1.webp'
+import image2 from '@/shared/assets/images/userProfile/photo2.webp'
+import image3 from '@/shared/assets/images/userProfile/photo3.webp'
+import image4 from '@/shared/assets/images/userProfile/photo4.webp'
+import image5 from '@/shared/assets/images/userProfile/photo5.webp'
+import image6 from '@/shared/assets/images/userProfile/photo6.webp'
+import image7 from '@/shared/assets/images/userProfile/photo7.webp'
+import Avatar from '@/shared/assets/images/userProfile/profileAvatar.webp'
+import { Typography } from '@/shared/ui/typography'
+import clsx from 'clsx'
+
 import s from './userProfile.module.scss'
+
+const galleryImages = [
+  image1.src,
+  image2.src,
+  image3.src,
+  image4.src,
+  image5.src,
+  image6.src,
+  image7.src,
+]
 
 export default function UserProfile() {
   return (
     <section className={s.userProfile}>
       <div className={s.info}>
-        <img alt={'Profile avatar'} className={s.ava} src={'./'} />
+        <img alt={'Profile avatar'} className={s.ava} src={Avatar.src} />
         <div className={s.bio}>
-          <header>
-            <h2 className={s.username}>URLProfile</h2>
-          </header>
+          <h2 className={s.username}>URLProfile</h2>
           <div className={s.stats}>
             <div>
               <span>2218</span>
-              <a href={'#'}>Following</a>
+              <Typography as={'a'} href={'#'}>
+                Following
+              </Typography>
             </div>
             <div>
               <span>2358</span>
-              <a href={'#'}>Followers</a>
+              <Typography as={'a'} href={'#'}>
+                Followers
+              </Typography>
             </div>
             <div>
               <span>2764</span>
-              <a href={'#'}>Publications</a>
+              <Typography as={'a'} href={'#'}>
+                Publications
+              </Typography>
             </div>
           </div>
-          <p className={s.description}>
+          <Typography as={'p'} className={clsx(s.description)} variant={'body1'}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          </p>
+            exercitation ullamco
+            <Typography as={'a'} variant={'link1'}>
+              laboris nisi ut aliquip ex ea commodo consequat.
+            </Typography>
+          </Typography>
         </div>
       </div>
       <div className={s.gallery}>
-        {Array.from({ length: 12 }).map((_, index) => (
-          <img
-            alt={`Gallery item ${index + 1}`}
-            className={s.galleryItem}
-            key={index}
-            src={`https://via.placeholder.com/234x228?text=Image+${index + 1}`}
-          />
-        ))}
+        {galleryImages.map((image, index) => {
+          return <img alt={'Gallery image'} key={index} src={image} />
+        })}
       </div>
     </section>
   )
