e94fbde131b2d77321520e232e19cbd7c6019aa0
dzlek <6894255@gmail.com>
2024-12-14 15:02:44 +0300

    feat: add publicPage registeredusers

--- src/views/publicPage/ui/PublicPage.tsx
+++ src/views/publicPage/ui/PublicPage.tsx
@@ -1,13 +1,25 @@
 'use client'
 
 import { Card } from '@/shared/ui/card'
+import { Typography } from '@/shared/ui/typography'
 
 import s from './publicPage.module.scss'
 
 function PublicPage() {
+  const numberOfUsers: string = '009213'
+
   return (
     <div className={s.wrapper}>
-      <div className={s.registeredUsers}>Registered users: 009213</div>
+      <div className={s.registeredUsers}>
+        <Typography variant={'h2'}>Registered users:</Typography>
+        <Typography className={s.registeredUserValue} variant={'h2'}>
+          {numberOfUsers.split('').map((number, i) => (
+            <span className={s.el} key={number + i}>
+              {number}
+            </span>
+          ))}
+        </Typography>
+      </div>
       <div className={s.cards}>
         <Card className={s.cardItem}>This is a default card</Card>
         <Card className={s.cardItem}>This is a default card</Card>
