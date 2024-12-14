068c9df0596aa1138f06c01e71e7657594d4d2fb
KristinOsmakov <oskristinov@gmail.com>
2024-12-14 20:01:05 +0300

    fix: fixed modalSlider

--- src/shared/ui/publicPageModal/modalSlider/ModalSlider.tsx
+++ src/shared/ui/publicPageModal/modalSlider/ModalSlider.tsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-
 import ArrowLeft from '@/shared/assets/icons/ArrowLeft'
 import ArrowRight from '@/shared/assets/icons/ArrowRight'
+import clsx from 'clsx'
 
 import s from './modalSlider.module.scss'
 
@@ -31,18 +30,14 @@ export const ModalSlider = ({
       <button className={s.prevButton} onClick={prevPost} type={'button'}>
         <ArrowLeft className={s.icon} />
       </button>
-      <img
-        alt={`Slide ${currentIndex + 1}`}
-        className={s.image}
-        src={posts[currentIndex].imagePost}
-      />
+      <img alt={'img'} className={s.image} src={posts[currentIndex].imagePost} />
       <button className={s.nextButton} onClick={nextPost} type={'button'}>
         <ArrowRight className={s.icon} />
       </button>
       <ul className={s.dots}>
         {posts.map((_, index) => (
           <li
-            className={`${s.dot} ${currentIndex === index ? s.active : ''}`}
+            className={clsx(s.dot, currentIndex === index && s.active)}
             key={index}
             onClick={() => goToSlide(index)}
           />
