f736f4a84bb34d04460d1ec6c026c11ac13db6df
dzlek <6894255@gmail.com>
2024-12-14 12:13:17 +0300

    feat: add publicPage

--- src/views/publicPage/ui/PublicPage.tsx
+++ src/views/publicPage/ui/PublicPage.tsx
@@ -1,7 +1,25 @@
 'use client'
 
-const PublicPage = () => {
-  return <div>PublicPage</div>
+import { Card } from '@/shared/ui/card'
+
+import s from './publicPage.module.scss'
+
+function PublicPage() {
+  return (
+    <div className={s.wrapper}>
+      <div className={s.registeredUsers}>Registered users: 009213</div>
+      <div className={s.cards}>
+        <Card className={s.cardItem}>This is a default card</Card>
+        <Card className={s.cardItem}>This is a default card</Card>
+        <Card className={s.cardItem}>This is a default card</Card>
+        <Card className={s.cardItem}>This is a default card</Card>
+        <Card className={s.cardItem}>This is a default card</Card>
+        <Card className={s.cardItem}>This is a default card</Card>
+        <Card className={s.cardItem}>This is a default card</Card>
+        <Card className={s.cardItem}>This is a default card</Card>
+      </div>
+    </div>
+  )
 }
 
 export default PublicPage
