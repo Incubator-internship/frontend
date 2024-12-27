4f9149b6dee9fa95af36cfa7c4d4522729726df0
Maria Kazakova <mariakazakova92@gmail.com>
2024-12-27 16:49:22 +0300

    feat aded styles for a scrollbar

--- src/views/messenger/ui/Messenger.tsx
+++ src/views/messenger/ui/Messenger.tsx
@@ -5,11 +5,45 @@ import { Typography } from '@/shared/ui/typography'
 import ChooseContact from '@/views/messenger/ui/chooseContact/ChooseContact'
 import Contact from '@/views/messenger/ui/contact/Contact'
 import { clsx } from 'clsx'
+import { string } from 'zod'
 
 import styles from './messenger.module.scss'
 
+interface ContactType {
+  img: string
+  message: string
+  name: string
+  time: string
+}
+
+const contacts: ContactType[] = [
+  { img: '/#', message: 'last message', name: 'Ekaterina Ivanova', time: 'time' },
+  { img: '/#', message: 'last message', name: 'Ivan', time: 'time' },
+  { img: '/#', message: 'last message', name: 'Petr', time: 'time' },
+  { img: '/#', message: 'last message', name: 'Mariya', time: 'time' },
+  { img: '/#', message: 'last message', name: 'Nataliya', time: 'time' },
+  { img: '/#', message: 'last message', name: 'Anna', time: 'time' },
+  { img: '/#', message: 'last message', name: 'Alex', time: 'time' },
+  { img: '/#', message: 'last message', name: 'Kseniya', time: 'time' },
+  { img: '/#', message: 'last message', name: 'Anatoli', time: 'time' },
+  { img: '/#', message: 'last message', name: 'Timyr', time: 'time' },
+  { img: '/#', message: 'last message', name: 'Daniil', time: 'time' },
+  { img: '/#', message: 'last message', name: 'Nikita', time: 'time' },
+]
+
 export default function Messenger() {
   const { data, isSuccess } = useGetMeQuery()
+  const contactList = contacts.map((contact: ContactType) => {
+    return (
+      <Contact
+        img={contact.img}
+        key={contact.name}
+        message={contact.message}
+        name={contact.name}
+        time={contact.time}
+      />
+    )
+  })
 
   return (
     <div style={{ display: 'flex', flexGrow: 1, margin: 0 }}>
@@ -23,14 +57,7 @@ export default function Messenger() {
                 <Input className={styles.search} placeholder={'Input search'} variant={'search'} />
               </div>
               <div className={clsx(styles.messagesContainer, styles.tableCell)}></div>
-              <div className={clsx(styles.tableCell)}>
-                <Contact
-                  img={'/#'}
-                  message={'last message'}
-                  name={'Ekaterina Ivanova'}
-                  time={'time'}
-                />
-              </div>
+              <div className={clsx(styles.tableCell)}>{contactList}</div>
               <div className={clsx(styles.center)}>
                 <ChooseContact />
               </div>
