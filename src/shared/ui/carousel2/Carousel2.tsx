3dc5bca03535a5db003eab433869b07aa1e30a54
KristinOsmakov <oskristinov@gmail.com>
2025-01-21 16:27:21 +0300

    .

--- src/shared/ui/carousel2/Carousel2.tsx
+++ src/shared/ui/carousel2/Carousel2.tsx
@@ -1,62 +1,24 @@
 'use client'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react'
 
-import ArrowLeft from '@/shared/assets/icons/ArrowLeft'
-import ArrowRight from '@/shared/assets/icons/ArrowRight'
-import clsx from 'clsx'
-import Image from 'next/image'
 
-import s from './carousel.module.scss'
-
-import { PostType } from '../../../views/publicPageModal/DataArray'
-
-type Carousel2Props = {
-  currentImageIndex: number
-  nextImage: () => void
-  post: PostType
-  prevImage: () => void
-  setCurrentIndex: (index: number) => void
-}
-
-export const Carousel2 = ({
-  currentImageIndex,
-  nextImage,
-  post,
-  prevImage,
-  setCurrentIndex,
-}: Carousel2Props) => {
-  const goToSlide = (index: number) => {
-    setCurrentIndex(index)
-  }
-
+export const Carousel2 = () => {
   return (
-    <div className={s.carousel}>
-      <Image
-        alt={'Image1'}
-        fill
-        priority
-        sizes={'600px'}
-        src={post.imagePost[currentImageIndex].imgPost}
-        style={{ objectFit: 'cover' }}
-      />
-      {post.imagePost.length > 1 && (
-        <>
-          <button className={s.nextButton} onClick={nextImage} type={'button'}>
-            <ArrowRight className={s.icon} />
-          </button>
-          <button className={s.prevButton} onClick={prevImage} type={'button'}>
-            <ArrowLeft className={s.icon} />
-          </button>
-        </>
-      )}
-      <ul className={s.dots}>
-        {post.imagePost.map((_, index) => (
-          <li
-            className={clsx(s.dot, currentImageIndex === index && s.active)}
-            key={index}
-            onClick={() => goToSlide(index)}
-          />
-        ))}
-      </ul>
-    </div>
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      navigation
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={swiper => console.log(swiper)}
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      slidesPerView={3}
+      spaceBetween={50}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
   )
 }
