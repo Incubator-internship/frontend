586ee201712274e17bbfff4743dadcab2ab72b55
Maria Kazakova <mariakazakova92@gmail.com>
2025-01-23 22:52:05 +0300

    feat:addedthe missing dependencies

--- src/features/addPost/ui/croppingPhotoStep/croppingPhotoStep.tsx
+++ src/features/addPost/ui/croppingPhotoStep/croppingPhotoStep.tsx
@@ -1,3 +1,5 @@
+import { Dispatch, SetStateAction } from 'react'
+
 import { FileWithPreview } from '@/features/addPost/ui/createPost/CreatePost'
 import { CroppingPhotoItem } from '@/features/addPost/ui/croppingPhotoStep/croppingPhotoItem/croppingPhotoItem'
 
@@ -5,16 +7,21 @@ import s from './croppingPhotoStep.module.scss'
 
 type Props = {
   images: FileWithPreview[]
+  setImageWithPreview: Dispatch<SetStateAction<FileWithPreview[]>>
 }
 
-export const CroppingPhotoStep = ({ images }: Props) => {
+export const CroppingPhotoStep = ({ images, setImageWithPreview }: Props) => {
   console.log('croppingImages', images)
 
   return (
     <div className={s.modalWrapp}>
       <div className={s.items}>
         {images.map(image => (
-          <CroppingPhotoItem image={image} key={image.id} />
+          <CroppingPhotoItem
+            image={image}
+            key={image.id}
+            setImageWithPreview={setImageWithPreview}
+          />
         ))}
       </div>
     </div>
