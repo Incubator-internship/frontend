f9ca6e48d53a6f5a064da929bcf85ce5eeeeed7e
dzlek <6894255@gmail.com>
2025-01-26 20:05:59 +0300

    feat: add date-fns

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,19 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {reactStrictMode: false};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: 'excubatoir-bucket.s3.eu-north-1.amazonaws.com',
+                port: '',
+                protocol: 'https',
+                search: '',
+            },
+        ],
+    },
+    reactStrictMode: false
+
+};
  
 export default withNextIntl(nextConfig);
