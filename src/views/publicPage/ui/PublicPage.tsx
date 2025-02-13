aef4f0f0c8d5a08ad8d5ac434dfceef4d0f95a9b
dzlek <6894255@gmail.com>
2025-02-13 12:42:24 +0300

    fix: fix publicPage

--- src/views/publicPage/ui/PublicPage.tsx
+++ src/views/publicPage/ui/PublicPage.tsx
@@ -36,6 +36,7 @@ const PublicPage: React.FC = () => {
     pollingInterval: 60000, // Update each 60sec
   })
 
+  console.log(posts)
   const refs = useRef<Array<ShowMoreRef | null>>([])
 
   const toggleLines: (index: number) => ShowMoreToggleLinesFn = index => e => {
@@ -74,7 +75,7 @@ const PublicPage: React.FC = () => {
       {error && <div>No posts available...</div>}
       {posts && (
         <div className={s.cards}>
-          {posts.map((card, i) => (
+          {posts.posts.map((card, i) => (
             <div className={s.cardItem} key={'cardItem' + i} onClick={() => handlePostClick(card)}>
               <div className={s.cardItemImage} onClick={e => e.stopPropagation()}>
                 <Swiper
