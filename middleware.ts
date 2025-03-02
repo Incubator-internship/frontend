0405e8b3f2f02c43ee6c87fbc0ff112944283062
Diana <m.diana.dev@gmail.com>
2025-03-02 16:07:54 +0300

    feat: update middleware for redirecting pages with links coming from the backend

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,35 @@
+import { routing } from '@/i18n/routing'
+import { NextRequest, NextResponse } from 'next/server'
 import createMiddleware from 'next-intl/middleware'
 
-import { routing } from './src/i18n/routing'
+const NEUTRAL_PAGES = ['/confirm-email', '/createnewpassword', '/authentication'] //ссылки на эти страницы приходят на почту с бекенда
 
-export default createMiddleware(routing)
+export function middleware(req: NextRequest) {
+  const { cookies, headers, nextUrl } = req
+
+  if (NEUTRAL_PAGES.includes(nextUrl.pathname)) {
+    const locale = getPreferredLocale(req)
+
+    return NextResponse.redirect(new URL(`/${locale}${nextUrl.pathname}${nextUrl.search}`, req.url))
+  }
+
+  return createMiddleware(routing)(req)
+}
+
+function getPreferredLocale(req: NextRequest) {
+  const cookieLocale = req.cookies.get('NEXT_LOCALE')?.value
+
+  if (cookieLocale) {
+    return cookieLocale
+  }
+
+  const acceptLang = req.headers.get('accept-language')?.split(',')[0].split('-')[0]
+
+  if (acceptLang) {
+    return ['en', 'ru'].includes(acceptLang) ? acceptLang : 'en'
+  }
+}
 
 export const config = {
-  matcher: ['/', '/(en|ru)/:path*'],
+  matcher: ['/((?!api|_next|favicon.ico).*)'],
 }
