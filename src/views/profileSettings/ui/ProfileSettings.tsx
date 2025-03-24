24b1358291a80cc09c62c0bd5ad01845a6dd11b3
Diana <m.diana.dev@gmail.com>
2025-03-24 13:33:35 +0300

    feat: add the General information section

--- src/views/profileSettings/ui/ProfileSettings.tsx
+++ src/views/profileSettings/ui/ProfileSettings.tsx
@@ -1,23 +1,46 @@
+'use client'
+import { ProfilePhoto } from '@/features/profilePhoto'
 import { Tabs } from '@/shared/ui/tabs'
+import { clsx } from 'clsx'
+
+import s from './profileSettings.module.scss'
 
 export default function ProfileSettings() {
   return (
     <div>
       <Tabs
+        className={s.tabs}
         defaultValue={'1'}
         tabs={[
           {
-            content: <div>General information Content</div>,
+            content: (
+              <div className={clsx(s.tabsContent, s.profileInformation)}>
+                <div className={s.profileInformationAvatar}>
+                  <ProfilePhoto />
+                </div>
+                <div className={s.profileInformationForm}>
+                  Profile form. Here will be a form for filling out a profile
+                </div>
+              </div>
+            ),
             title: 'General information',
             value: '1',
           },
-          { content: <div>Devices Content</div>, title: 'Devices', value: '2' },
           {
-            content: <div>Account Management Content</div>,
+            content: <div className={s.tabsContent}>Devices Content</div>,
+            title: 'Devices',
+            value: '2',
+          },
+          {
+            content: <div className={s.tabsContent}>Account Management Content</div>,
             title: 'Account Management',
             value: '3',
           },
-          { content: <div>My payments Content</div>, title: 'My payments', value: '4' },
+          {
+            content: <div className={s.tabsContent}>My payments Content</div>,
+            title: 'My payments',
+            value: '4',
+          },
         ]}
       />
     </div>
