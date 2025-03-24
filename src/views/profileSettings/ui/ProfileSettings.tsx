07ca051d4d89caf3290ff7d63285d33346771f96
Diana <m.diana.dev@gmail.com>
2025-03-24 13:04:00 +0300

    fix: fix tabs component

--- src/views/profileSettings/ui/ProfileSettings.tsx
+++ src/views/profileSettings/ui/ProfileSettings.tsx
@@ -1,3 +1,25 @@
+import { Tabs } from '@/shared/ui/tabs'
+
 export default function ProfileSettings() {
-  return <div></div>
+  return (
+    <div>
+      <Tabs
+        defaultValue={'1'}
+        tabs={[
+          {
+            content: <div>General information Content</div>,
+            title: 'General information',
+            value: '1',
+          },
+          { content: <div>Devices Content</div>, title: 'Devices', value: '2' },
+          {
+            content: <div>Account Management Content</div>,
+            title: 'Account Management',
+            value: '3',
+          },
+          { content: <div>My payments Content</div>, title: 'My payments', value: '4' },
+        ]}
+      />
+    </div>
+  )
 }
