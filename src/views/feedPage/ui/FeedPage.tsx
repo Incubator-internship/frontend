3e6ab7a361b3d36b844929a18e5d65ba413544ce
Kris Osmakov <krisosmakov@MacBook-Air-Kris.local>
2025-05-08 23:33:04 +0300

    feat: replaced static data (not all)

--- src/views/feedPage/ui/FeedPage.tsx
+++ src/views/feedPage/ui/FeedPage.tsx
@@ -1,18 +1,27 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 
+import { useGetMeQuery } from '@/app/api/auth/authApi'
 import { useGetPostsUserIdQuery } from '@/app/api/posts/postsApi'
+import { useGetProfileQuery } from '@/app/api/users/usersApi'
 import {
   BookmarkOutlineIcon,
   HeartOutlineIcon,
   MessageCircleOutlineIcon,
   PaperPlaneOutlineIcon,
 } from '@/shared/assets/icons'
+import PensilIcon from '@/shared/assets/icons/PensilIcon'
+import TrashIcon from '@/shared/assets/icons/TrashIcon'
 import avatar1 from '@/shared/assets/images/avatars/avatar1.webp'
 import { AddCommentBlock } from '@/shared/ui/addCommentBlock/AddCommentBlock'
 import { Avatar, AvatarFallback, AvatarImage } from '@/shared/ui/avatar/Avatar'
+import DropdownMenuDemo from '@/shared/ui/dropdownMenu/DropdownMenu'
+import { Modal } from '@/shared/ui/modal'
+import { ModalCloseDeleteUnfollowPost } from '@/shared/ui/modal/modalCreateOrDeletePost/ModalCloseDeleteUnfollowPost'
 import { Typography } from '@/shared/ui/typography'
+import { UpdatePostModal } from '@/views/publicPageModal/ui/updatePostModal/UpdatePostModal'
+import { skipToken } from '@reduxjs/toolkit/query'
 import { formatDistanceToNow } from 'date-fns'
 import { enGB, ru } from 'date-fns/locale'
 import { useTranslations } from 'next-intl'
@@ -24,20 +33,30 @@ const FeedPage: React.FC = () => {
 
   const t = useTranslations<'FeedPage'>('FeedPage')
 
-  const { data: posts, error, isLoading } = useGetPostsUserIdQuery(7)
+  const [isOpenModal, setIsOpenModal] = useState(false)
+  const [isOpenEditModal, setIsOpenEditModal] = useState(false)
+  const { data: meData } = useGetMeQuery()
+  const { data: posts, error, isLoading } = useGetPostsUserIdQuery(meData?.userId ?? skipToken)
+  const { data: dataUser } = useGetProfileQuery(meData?.userId ?? skipToken)
+  const nickName = meData?.login
+  const avatarSmall = dataUser?.smallAvatarUrl
 
   const updatedAt = posts && posts[0]?.updatedAt
+
+  const handleDeletePost = () => {
+    setIsOpenModal(false)
+  }
 
   return (
     <div style={{ display: 'flex' }}>
       <div className={s.postWrapper}>
         <div className={s.postAvatarTitle}>
           <Avatar>
-            <AvatarImage alt={'Avatar1'} src={avatar1.src} />
+            <AvatarImage alt={'Avatar1'} src={avatarSmall} />
             <AvatarFallback>AF</AvatarFallback>
           </Avatar>
 
-          <Typography variant={'h3'}>URLProfiele &middot;</Typography>
+          <Typography variant={'h3'}>{nickName}</Typography>
           {updatedAt && (
             <Typography color={'grey'} variant={'smallText'}>
               {formatDistanceToNow(new Date(updatedAt), {
@@ -47,8 +66,39 @@ const FeedPage: React.FC = () => {
             </Typography>
           )}
           <Typography className={s.lastChild} variant={'h2'}>
-            &middot;&middot;&middot;
+            <DropdownMenuDemo
+              content={[
+                {
+                  icon: <PensilIcon />,
+                  label: 'Edit Post',
+                  onSelect: () => setIsOpenEditModal(true),
+                },
+                {
+                  icon: <TrashIcon />,
+                  label: 'Delete Post',
+                  onSelect: () => setIsOpenModal(true),
+                },
+              ]}
+            />
           </Typography>
+          {isOpenModal && (
+            <ModalCloseDeleteUnfollowPost
+              isOpenModal={isOpenModal}
+              onCloseModal={() => setIsOpenModal(false)}
+              onDelete={handleDeletePost}
+              variant={'delete'}
+            />
+          )}
+          {isOpenEditModal && (
+            <Modal
+              className={s.editModal}
+              isOpen={isOpenEditModal}
+              onClose={() => setIsOpenEditModal(false)}
+              title={'Edit Post'}
+            >
+              <UpdatePostModal setIsOpenEditModal={setIsOpenEditModal} />
+            </Modal>
+          )}
         </div>
 
         {posts && <img alt={'Image1'} className={s.postImage} src={posts[0].photos[0].url} />}
@@ -62,10 +112,12 @@ const FeedPage: React.FC = () => {
           </div>
           <div className={s.postContent}>
             <Avatar>
-              <AvatarImage alt={'Avatar1'} src={avatar1.src} />
+              <AvatarImage alt={'Avatar1'} src={avatarSmall} />
               <AvatarFallback>AF</AvatarFallback>
             </Avatar>
-            <Typography variant={'boldText14'}>URLProfile {posts && posts[0]?.content}</Typography>
+            <Typography variant={'boldText14'}>
+              {nickName} {posts && posts[0]?.content}
+            </Typography>
           </div>
           <div className={s.postLikes}>
             {[1, 2, 3].map(item => (
